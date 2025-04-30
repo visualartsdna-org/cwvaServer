229406a40b5b7448e3bd08c52a616c867aa673ad
rspates <rspates@users.noreply.github.com>
2025-04-30 07:41:02 -0400

    Revised DOT drivers

--- src/main/java/cwva/DotDriver.groovy
+++ src/main/java/cwva/DotDriver.groovy
@@ -2,6 +2,7 @@ package cwva
 
 import rdf.JenaUtilities
 import util.Tmp
+import services.Svg2Html
 
 class DotDriver extends BaseDriver {
 
@@ -11,9 +12,8 @@ class DotDriver extends BaseDriver {
 			case "svg":
 				def svg = Tmp.getTemp(".svg")
 				def dot = Tmp.getTemp(".dot")
-				def args = toArgs("""
-				   -ttl $ttl -dot $dot -html $html -svg $svg""")
-				services.OntoToDotDriver.main(args)
+				services.OntoToDotDef.driver(ttl,dot)
+				drive(ttl, dot, html, svg,"VisualArtsDNA Ontology")
 				Tmp.delTemp(svg)
 				Tmp.delTemp(dot)
 				break
@@ -25,20 +25,19 @@ class DotDriver extends BaseDriver {
 				def ttlTmp = Tmp.getTemp(".ttl")
 				def m = ju.loadFiles(ttl)
 				ju.saveModelFile(m, ttlTmp, "TTL")
-				def args = toArgs("""
-				   -ttl $ttlTmp -dot $dot -html $html -svg $svg""")
-				services.VocabToDotDriver.main(args)
+				services.VocabToDotDef.driver(ttl,dot)
+				drive(ttl, dot, html, svg,"VisualArtsDNA Thesaurus")
 				Tmp.delTemp(svg)
 				Tmp.delTemp(dot)
 				Tmp.delTemp(ttlTmp)
 				break
 
 			case "svgImpOnto":
+				throw new RuntimeException("svgImpOnto not implemented")
+				
 				def svg = Tmp.getTemp(".svg")
 				def dot = Tmp.getTemp(".dot")
-				def args = toArgs("""
-				   -ttl $ttl -dot $dot -html $html -svg $svg""")
-				services.InstEffectOntoToDotDriver.main(args)
+				services.InstEffectOntoToDotDriver.drive(ttl, dot, html, svg)
 				Tmp.delTemp(svg)
 				Tmp.delTemp(dot)
 				break
@@ -46,6 +45,22 @@ class DotDriver extends BaseDriver {
 			default:
 				break
 		}
+	}
+	
+	def drive(ttl, dot, html, svg, title) {
+		
+		// dot -Tsvg -o test100.svg test100.dot
+		Process process = "dot -Tsvg -o $svg $dot".execute()
+		def out = new StringBuffer()
+		def err = new StringBuffer()
+		process.consumeProcessOutput( out, err )
+		process.waitFor()
+		if( err.size() > 0 ) throw new Exception( ""+err)
+		if( out.size() > 0 ) println "$out"
+		
+		def s2h = new Svg2Html()
+		s2h.convert(html,svg,title)
+
 	}
 	
 }
