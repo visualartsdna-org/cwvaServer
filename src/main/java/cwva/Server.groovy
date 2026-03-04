9fea406cebdf1eeff8192f8cbdd97fdd8e2aeed7
rspates <rspates@users.noreply.github.com>
2026-03-04 11:44:31 -0500

    new context handler for CORS

--- src/main/java/cwva/Server.groovy
+++ src/main/java/cwva/Server.groovy
@@ -1,8 +1,13 @@
 package cwva
 
 import java.text.SimpleDateFormat
+import org.eclipse.jetty.servlet.FilterHolder
+import org.eclipse.jetty.servlet.ServletContextHandler
 import org.eclipse.jetty.servlet.ServletHandler;
 import rdf.util.DBMgr
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
 
 // singleton
 class Server {
@@ -42,16 +47,35 @@ class Server {
 	}
 
 	def startJetty(port,theClass) {
-
 		def server = new org.eclipse.jetty.server.Server(port)
-
-		def handler = new ServletHandler();
-		server.setHandler(handler);
-
-		handler.addServletWithMapping(theClass, "/*");
-		server.start();
-		server.join();
+		
+		def context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS)
+		context.setContextPath("/")
+		
+		// Add CORS filter
+		def cors = context.addFilter(CrossOriginFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST))
+		cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*")
+		cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD,OPTIONS")
+		cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin")
+		
+		// Your existing servlet mapping
+		context.addServlet(theClass, "/*")
+		
+		server.setHandler(context)
+		server.start()
+		server.join()
 	}
+//	def startJetty(port,theClass) {
+//
+//		def server = new org.eclipse.jetty.server.Server(port)
+//
+//		def handler = new ServletHandler();
+//		server.setHandler(handler);
+//		
+//		handler.addServletWithMapping(theClass, "/*");
+//		server.start();
+//		server.join();
+//	}
 	
 	static def rehost(r) {
 		if (r instanceof List) {
